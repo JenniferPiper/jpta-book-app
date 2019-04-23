93430243643b6d34f21faf3ba432524289a252e1
Jennifer Piper <jennifer@squareandplum.com>
2019-04-23 12:17:36 -0700

    render list of 10 book titles on searches page

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 'use strict';
 
-const express  = require('express');
+const express = require('express');
+const superagent = require('superagent');
 
 const app = express();
 
 const PORT = process.env.PORT || 3500;
 
 app.use(express.static('./public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view-engine', 'ejs');
 
-let listOfNumbers = [1,2,3,4,5];
+//let listOfNumbers = [1, 2, 3, 4, 5];
+
+
+function Book(rawBookData){
+  this.title = rawBookData.volumeInfo.title;
+}
 
 app.get('/', (request, response) => {
-  response.render('pages/index.ejs', {listOfNumbers: listOfNumbers});
+  response.render('pages/index.ejs');
 
 });
 
 app.post('/searches', (request, response) => {
-  response.render('pages/index.ejs', {listOfNumbers: listOfNumbers});
+  const booksArray = [];
+  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=+intitle:${request.body.search[0]}`)
+    .then(result => {
+      for (let i = 0; i < 10 ; i++) {
+        booksArray.push(new Book(result.body.items[i]));
+      }
+      console.log(booksArray);
+
+       response.render('pages/searches/show.ejs', { searchResults: booksArray});
+      //console.log('search results: ', result);
+    })
 
 });
 
