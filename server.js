bf703c84cecedcebc154ad32d10d6737e3e0678f
Jennifer Piper <jennifer@squareandplum.com>
2019-04-23 11:13:58 -0700

    set up book app form template

--- server.js
+++ server.js
@@ -6,15 +6,22 @@ const app = express();
 
 const PORT = process.env.PORT || 3500;
 
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended:true}));
 app.set('view-engine', 'ejs');
 
-let list = [1,2,3,4,5];
+let listOfNumbers = [1,2,3,4,5];
 
 app.get('/', (request, response) => {
-  response.render('pages/index.ejs');
+  response.render('pages/index.ejs', {listOfNumbers: listOfNumbers});
 
 });
 
-app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+app.post('/searches', (request, response) => {
+  response.render('pages/index.ejs', {listOfNumbers: listOfNumbers});
+
+});
+
+app.use('*', (request, response) => response.status(404).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`server is up on port ${PORT}`));
